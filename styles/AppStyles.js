00fc0a8430e9f82e9a3f278212380b9255205d23
Qiqi Yang <qyang315@gatech.edu>
2022-01-29 17:37:57 -0500

    feat: Add styles to Fitbit UI

---
+++ styles/AppStyles.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+    row: {
+        margin: 10,
+        flex: 1,
+        flexDirection: 'row',
+    },
+    label: {
+        color: '#007FFF',
+    },
+    button: {
+        marginTop: 20,
+        marginLeft: 24,
+        borderRadius: 5,
+        height: 50,
+        width: 150,
+        backgroundColor: '#FDC7C7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    statList: {
+        marginTop: 20,
+        marginHorizontal: 18,
+    },
+})
+
+export default styles
