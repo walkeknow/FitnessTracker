9411e14de632e35b14d7ee42807a21b1bed0a3dc
Kewal Shah <kewalshah077@gmail.com>
2022-01-29 16:56:54 -0500

    feat: integrate Fitbit API

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {Node} from 'react';
+import qs from 'qs';
+import config from './config.js';
 import {
+  Button,
+  Linking,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -25,65 +22,127 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import moment from 'moment';
+
+function OAuth(client_id, getData, setDataObj) {
+  const subscription = Linking.addEventListener('url', handleUrl);
+  function handleUrl(event) {
+    console.log('event URL', event.url);
+    subscription.remove();
+    const [, query_string] = event.url.match(/\#(.*)/);
+    console.log('query string', query_string);
+    const query = qs.parse(query_string);
+    console.log(`query: ${JSON.stringify(query)}`);
+    getData(query.access_token, setDataObj);
+  }
+
+  const oauthurl = `https://www.fitbit.com/oauth2/authorize?${qs.stringify({
+    client_id,
+    response_type: 'token',
+    scope: 'heartrate activity profile sleep weight',
+    redirect_uri: 'fitbit://fit',
+    expires_in: '31536000',
+  })}`;
+  console.log('oAuth URL', oauthurl);
+  Linking.openURL(oauthurl).catch(err =>
+    console.error('Error processing linking', err),
+  );
+}
+
+async function getData(access_token, setDataObj) {
+  const urlParameter = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+
+  const today = moment().format('YYYY-MM-DD');
+
+  const urls = [
+    `https://api.fitbit.com/1/user/-/activities/heart/date/${today}/7d.json`,
+    `https://api.fitbit.com/1/user/-/activities/date/${today}.json`,
+    `https://api.fitbit.com/1/user/-/body/log/weight/date/${today}/30d.json`,
+    `https://api.fitbit.com/1.2/user/-/sleep/date/${today}.json`,
+    `https://api.fitbit.com/1/user/-/body/log/weight/goal.json`,
+  ];
+
+  try {
+    const responses = await Promise.all(
+      urls.map(url => fetch(url, urlParameter)),
+    );
+    const responsesJson = await Promise.all(responses.map(resp => resp.json()));
+    console.log('RESPONSE', responsesJson);
+    console.log('HEARTRESP', responsesJson[3]?.sleep?.[0]?.levels);
+    setDataObj({
+      'Daily Step Goal': responsesJson[1]?.goals?.steps ?? 0,
+      'Steps Taken Today': responsesJson[1]?.summary?.steps ?? 0,
+      'Total Calories Burnt': responsesJson[1]?.goals?.caloriesOut ?? 0,
+      'Calories Burnt Today': responsesJson[1]?.summary?.caloriesOut ?? 0,
+      'Weight Goal (kg)': responsesJson[4]?.goal?.weight ?? 0,
+      'Weight (kg)': responsesJson[2]?.weight?.[0]?.weight ?? 0,
+      'Total Hours in Bed Today':
+        (responsesJson[3]?.sleep?.[0]?.timeInBed / 60).toFixed(1) ?? 0,
+      'Minutes Awake':
+        responsesJson[3]?.sleep?.[0]?.levels?.summary?.wake?.minutes ?? 0,
+      'Minutes of Light Sleep':
+        responsesJson[3]?.sleep?.[0]?.levels?.summary?.light?.minutes ?? 0,
+      'Minutes of Deep Sleep':
+        responsesJson[3]?.sleep?.[0]?.levels?.summary?.deep?.minutes ?? 0,
+      'Minutes of Rem Sleep':
+        responsesJson[3]?.sleep?.[0]?.levels?.summary?.rem?.minutes ?? 0,
+    });
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+}
 
 const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <Text>{title}</Text>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+const HealthStats = ({parameter, value}) => {
+  return (
+    <View>
+      <Text>{`${parameter}: `}</Text>
+      <Text>{value}</Text>
     </View>
   );
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: Colors.lighter,
   };
+  const [dataObj, setDataObj] = useState();
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            flex: 1,
+            backgroundColor: Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="Fitbit Demo">Fitbit stats fetched from API:</Section>
+          <Pressable
+            onPress={() => OAuth(config.client_id, getData, setDataObj)}>
+            <Text>Integrate Fitbit</Text>
+          </Pressable>
+          <View>
+            {dataObj &&
+              Object.entries(dataObj).map(([k, v]) => (
+                <HealthStats key={k} parameter={k} value={v} />
+              ))}
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -94,18 +153,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  client_id: '23BCHD',
+  client_secret: 'a4e95058819c8ab71c83d8b00e7a28f2T',
+};
